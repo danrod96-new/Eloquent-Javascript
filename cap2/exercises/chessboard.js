bc31410e39d74ffc32da7bae7edbf60c93def6e5
Daniel Rodriguez <danrod@gmail.com>
2025-12-04 14:51:53 -0500

    Exercise updates for the Chapter 2

--- cap2/exercises/chessboard.js
+++ cap2/exercises/chessboard.js
@@ -1,3 +1,11 @@
+/* 
+ * Exercise 3:
+ *
+ * Write a program that creates a string that represents an 8×8 grid, using newline characters to separate lines. 
+  At each position of the grid there is either a space or a "#" character. The characters should form a chessboard.
+ *
+ * My solution:
+ */
 
 let size = 16;
 
--- cap2/exercises/triangle.js
+++ cap2/exercises/triangle.js
@@ -1,11 +1,26 @@
+/* 
+ * Exercise 1:
+ *
+ * Write a loop that makes seven calls to console.log to output the following triangle:
+ *
+ * #
+ * ##
+ * ###
+ * ####
+ * #####
+ * ######
+ * #######
+ */
 
 let str = "";
 
+/* My solution */
 for (let count = 0; count < 7; count++) {
  str += "#";
  console.log(str);
 }
 
+/* Book's solution */
 for (let line = "#"; line.length < 8; line += "#") {
   console.log(line);
 }

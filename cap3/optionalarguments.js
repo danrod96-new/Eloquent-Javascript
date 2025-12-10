bc579ed7fb4858e5817bc73aadacb90f95d0335f
Daniel Rodriguez <danrod@gmail.com>
2025-12-10 07:44:32 -0500

    Added comments in my JS samples

--- cap3/optionalarguments.js
+++ cap3/optionalarguments.js
@@ -1,4 +1,8 @@
 
+/* JavaScript is extremely broad-minded about the number of arguments you can pass to a function. 
+ * If you pass too many, the extra ones are ignored. If you pass too few, the missing parameters are 
+ * assigned the value undefined. */
+
 function square(x) { return x * x; }
 
 console.log(square(4, 2, "aaa"));
@@ -27,3 +31,11 @@ function power(base, exponent = 2) {
 console.log(power(4));
 
 console.log(power(2, 6));
+
+/* There is a way in which a function body can get at the whole list of arguments it was passed. 
+ * This is helpful because it allows a function to accept any number of arguments. For example, 
+ * console.log does this, outputting all the values it is given 
+ */
+
+console.log("console.log outputting all the values it is given:")
+console.log("C", 0, 2.2)

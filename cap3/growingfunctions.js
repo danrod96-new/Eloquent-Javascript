3f8d01437b350e8d3f34f37cf4c89fa773bb5acf
Daniel Rodriguez <danrod@gmail.com>
2025-12-25 14:32:16 -0500

    Added extra comments in the JS sample files and the puzzle exercise

--- cap3/growingfunctions.js
+++ cap3/growingfunctions.js
@@ -1,4 +1,10 @@
 
+/*
+ * We want to write a program that prints two numbers: the numbers of cows and chickens on a farm, with the words Cows 
+ * and Chickens after them and zeros padded before both numbers so that they are always three digits long.
+ */
+
+/* First attempt at writing the function */
 function printFarmInventory(cows, chickens) {
   let cowString = String(cows);
 
@@ -17,6 +23,7 @@ function printFarmInventory(cows, chickens) {
   console.log(`${chickenString} Chickens`);
 }
 
+console.log("First attempt at writing the function")
 printFarmInventory(44, 10);
 
 function printZeroPaddedWithLabel(number, label) {
@@ -35,8 +42,10 @@ function printFarmInventory2(cows, chickens, pigs) {
   printZeroPaddedWithLabel(pigs, "pigs");
 }
 
+console.log("Second attempt at writing the function")
 printFarmInventory2(4, 2, 4);
 
+/* Simplified way of writing the function */
 function zeroPad(number, width) {
   let string = String(number);
 
@@ -53,4 +62,5 @@ function printFarmInventory3(cows, chickens, pigs) {
   console.log(`${zeroPad(pigs, 3)} Pigs`);
 }
 
+console.log("Same ")
 printFarmInventory3(7, 13, 1);
--- cap3/recursion.js
+++ cap3/recursion.js
@@ -1,8 +1,14 @@
+/*
+ * It is perfectly okay for a function to call itself, as long as it doesn’t do it so often that it overflows the stack. 
+ * A function that calls itself is called recursive. Recursion allows some functions to be written in a different style. Take, 
+ * for example, this power function, which does the same as the ** (exponentiation) operator.
+ */
 
 function power(base, exponent) {
   if (exponent == 0) {
     return 1;
   } else {
+    console.log("Current value of exponent is: ", exponent);
     return base * power(base, exponent - 1);
   }
 }

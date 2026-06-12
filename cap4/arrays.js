034dda0e840cbcd0c7f22c48aaff585dfc57d600
Daniel Rodriguez <danrod@gmail.com>
2026-06-12 19:36:32 -0400

    Added extra comments and more sample code for learning purposes

--- cap4/arrays.js
+++ cap4/arrays.js
@@ -1,8 +1,14 @@
-
+/* Fortunately, JavaScript provides a data type specifically for storing sequences of values. 
+ * It is called an array and is written as a list of values between square brackets, 
+ * separated by commas. */
 let listOfNumbers = [2, 3, 5, 7, 11];
 
 console.log(listOfNumbers[2]);
 console.log(listOfNumbers[0]);
 console.log(listOfNumbers[2 - 1]);
 
-console.log("Lenght of Array is: " + listOfNumbers.length);
+console.log("Length of Array is: " + listOfNumbers.length);
+
+for (let i = 0; i < listOfNumbers.length; i++) {
+  console.log("Number is: " + listOfNumbers[i]);
+}
--- cap4/methods.js
+++ cap4/methods.js
@@ -1,15 +1,24 @@
-
+/* Both string and array values contain, in addition to the length property, 
+ * a number of properties that hold function values */
 let doh = "Choma";
 
+//Get data type of variable doh
 console.log(typeof doh.toUpperCase());
 
+/* Every string has a toUpperCase property. When called, it will return a copy of the string in 
+ * which all letters have been converted to uppercase. There is also toLowerCase, going the other way. */
+
+//Convert doh to upper case
 console.log(doh.toUpperCase());
 
+//Convert doh to lower case
 console.log(doh.toLowerCase());
 
+/* This example demonstrates two methods you can use to manipulate arrays. */
 let sequence = [1, 2, 3];
 
 // adding values to the end of array
+/* The push method adds values to the end of an array. */
 sequence.push(4);
 
 sequence.push(5);
@@ -17,6 +26,7 @@ sequence.push(5);
 console.log(sequence);
 
 // remove values from the end of array
+/* The pop method does the opposite, removing the last value in the array and returning it */
 console.log(sequence.pop());
 
 console.log(sequence);

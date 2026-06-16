a098e873a33acfa7b3bc84bffb2c482b2838a14f
Daniel Rodriguez <danrod@gmail.com>
2026-06-16 07:35:18 -0400

    More comments in the mutability.js sample file

--- cap4/mutability.js
+++ cap4/mutability.js
@@ -17,23 +17,34 @@ object1.value = 15;
 console.log(object2.value);
 console.log(object3.value);
 
+/* Bindings can also be changeable or constant, but this is separate from the way their values behave. 
+ * Even though number values don’t change, you can use a let binding to keep track of a changing number 
+ * by changing the value at which the binding points. Similarly, though a const binding to an object 
+ * can itself not be changed and will continue to point at the same object, the contents of that object
+ * might change. */
 const score = {visitors: 0, home: 0};
 //this is ok
 score.visitors = 1;
 // this is not ok
 // score = {visitors: 1, home: 1};
 
+/* journal will hold an array of objects */
 let journal = [];
 
+/* function that adds a log entry to the journal */
 function addEntry(events, squirrel) {
   journal.push({events, squirrel});
 }
 
+/* Adding a few entries */
 addEntry(["work", "tele", "pizza", "beer"], false);
 addEntry(["weekend", "tree", "peanuts", "beer"], true);
-addEntry(["broccoli", "cheetos", "biking"], false);
+addEntry(["broccoli", "cheetos", "biking", "writing"], false);
 addEntry(["weekend", "cycling", "break", "peanuts", "beer"], true);
 
+/* This is the function that computes the phi coefficient from a flat array [1, 2, 3, 4] 
+ * The phi coefficient measures how close the variables are related: 1 means that 
+ * are related, 0 are not, -1 are related but opposite */
 function phi(table) {
   return (table[3] * table[0] - table[2] * table[1]) /
   Math.sqrt((table[2] + table[3]) *
@@ -42,10 +53,15 @@ function phi(table) {
             (table[0] + table[2]));
 }
 
+/* To extract a two-by-two table for a specific event from the journal, we must loop over all 
+ * the entries and tally how many times the event occurs in relation to squirrel transformations */
 function tableFor(event, journal) {
   let table = [0, 0, 0, 0];
   for (let i = 0; i < journal.length; i++) {
     let entry = journal[i], index = 0;
+    /* Arrays have an includes method that checks whether a given value exists in the array. The 
+     * function uses that to determine whether the event name it is interested in is part of the 
+     * event list for a given day. */
     if (entry.events.includes(event)) index += 1;
     if (entry.squirrel) index += 2;
     table[index] += 1;
